483065149b56d40d4f26df6bc0be84db3dac64b8
Mariana Petropoulou <marianapetropoulou@gmail.com>
2024-02-21 21:27:40 +0100

    attempt for user auth

---
+++ cognito.js
@@ -0,0 +1,38 @@
+const AmazonCognitoId = require("amazon-cognito-identity-js");
+const AWS = require("aws-sdk");
+const request = require("request");
+const jwkToPem = require("jwk-to-pem");
+const jwt = require("jsonwebtoken");
+
+//global.fetch = require("node-fetch");
+
+
+const poolData = {
+UserPoolId: "eu-north-1_R9M5he9af",
+ClientId: "1tk5am2ea91tjfvn2aq0ssdt7v"
+};
+const aws_region = "eu-north-1";
+
+const CognitoUserPool = AmazonCognitoId.CognitoUserPool;
+const userPool = new AmazonCognitoId.CognitoUserPool (poolData);
+//Register a new user
+const signUp = (name, email, password) => {
+    return new Promise ((result, reject) => {
+        try {
+            //Create an attribute list.
+            const attributeList = [];
+            attributeList.push(new AmazonCognitoId.CognitoUserAttribute({Name:"name", Value: name}));
+            attributeList.push(new AmazonCognitoId.CognitoUserAttribute({Name:"email", Value : email}));
+            userPool.signUp(email,password,attributeList,null, (err,data)=> {
+                if (err) reject(err);
+                else result(data);
+            } );
+        }
+        catch(err){
+            reject(err);
+        }
+    });
+}
+
+module.exports.signUp = signUp;
+
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user/:userId', function(req, res, next) {
+  var userId = req.params.userId;
+  res.send(`User with id: ${userId}`);
 });
 
 module.exports = router;
