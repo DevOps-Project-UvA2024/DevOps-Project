e4cdf6f339a400684d3f13f6277f651543b51123
Dimitris Bratsos <jimbratsosvine@gmail.com>
2024-02-22 15:19:05 +0100

    Update start_server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd /var/www/html/devopsportal
-npm start &
+
+# Start the application with PM2
+pm2 start app.js --name "devopsportal"
+
+# Save the process list for resurrection on reboot
+pm2 save
+
+# Set up PM2 to restart on system boot
+pm2 startup
