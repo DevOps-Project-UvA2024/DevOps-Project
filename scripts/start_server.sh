f8f1c6fdaff62be9ec126dfd43d7afe256ab1563
Dimitris Bratsos <jimbratsosvine@gmail.com>
2024-02-22 19:38:52 +0100

    Change Scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Navigate to your application directory (if necessary)
 cd /var/www/html/devopsportal
-nohup npm run start &
+
+# Start the application with PM2
+sudo pm2 start npm --name "eduapp" -- run start
+
+# Save the PM2 list
+sudo pm2 save
