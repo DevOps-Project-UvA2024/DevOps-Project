48efefb70b8f4444db7d2af4d00fba992b1427a5
Dimitris Bratsos <jimbratsosvine@gmail.com>
2024-02-22 18:28:13 +0100

    Script fix

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,3 +3,4 @@ cd /var/www/html/devopsportal
 sudo apt update
 sudo apt install nodejs npm -y
 npm install
+sudo npm install pm2 -g
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/html/devopsportal
-npm run start &
+pm2 start npm --name "eduapp" -- run start
