9b4ff78875acff2596609f278d92bdf42ce679ed
Dimitris Bratsos <jimbratsosvine@gmail.com>
2024-02-23 10:57:48 +0100

    Added ambercent to pm2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html/devopsportal
 # Start the application with PM2
-pm2 start npm --name "eduapp" -- run start
+pm2 start npm --name "eduapp" -- run start &
 
 # Save the PM2 list
 pm2 save
