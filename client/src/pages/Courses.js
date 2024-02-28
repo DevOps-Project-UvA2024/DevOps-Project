cb8cdd1ec82823dface18b2519eb3ac9cb9d03f4
Mariana Petropoulou <marianapetropoulou@gmail.com>
2024-02-28 18:10:08 +0100

    file page UI done

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -27,13 +27,14 @@ const Courses = () => {
   };
 import { PlusCircleTwoTone } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Form, Input, Alert, Space, Table, Tag } from 'antd';
 
 
 
 const Courses = () => {
 
   const navigate = useNavigate();
+  const {TextArea} = Input;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -90,8 +91,27 @@ const Courses = () => {
           <Button type="primary" onClick={showModal}>
             Add course 
           </Button>
-          <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            
+          <Modal title="Add a new course" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <Form>
+              <Form.Item
+                label="Course Name"
+                name="course"
+                rules={[{ required: true, message: 'Please enter the course\'s name' }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Department"
+                name="department"
+                rules={[{ required: true, message: 'Please input the department of the course!' }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Description"
+                name="description"
+                rules={[{ required: true, message: 'Please enter a description!' }]}>
+                <TextArea rows={4} placeholder="Enter a description for the course"/>
+              </Form.Item>
+            </Form>
           </Modal>
         </div>
         <Table columns={columns} dataSource={data}/>
