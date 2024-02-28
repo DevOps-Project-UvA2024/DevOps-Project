fdd4232b289c6d2bd2b80baa12353e67922fdf45
Mariana Petropoulou <marianapetropoulou@gmail.com>
2024-02-28 18:10:08 +0100

    file page UI done

--- client/src/pages/Course.js
+++ client/src/pages/Course.js
@@ -1,64 +1,144 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import Reactz,  { useState } from 'react';
+import { Space, Table, Button, Rate, Checkbox , Modal, Form, Input} from 'antd';
+import { DownloadOutlined,StarOutlined } from '@ant-design/icons';
 import "../styles/tables_style.css"
-import { PlusCircleTwoTone } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
-
-
 
 
 const Course = () => {
-    const navigate = useNavigate();
+    
+    //const navigate = useNavigate();
+
+    const onChange = (checked, recordKey) => {
+      console.log(`checked = ${checked}, key = ${recordKey}`);
+    };
+
+    // modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedName, setSelectedName] = useState('');
+    const [modalRating, setModalRating] = useState(0); 
+
+    const showNameModal = (name, yourate) => {
+      setSelectedName(name);
+      setModalRating(yourate);
+      setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
+
   
     const columns = [
       {
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
-        render: (text, record) => <a onClick={() => navigate(`/courses/course`)}>{text}</a>,
+        sorter: (a, b) => a.name.localeCompare(b.name),
       },
       {
-        title: 'Department',
-        dataIndex: 'department',
-        key: 'department',
+        title: 'Uploaded By',
+        dataIndex: 'uploadedby',
+        key: 'uploadedby',
+        sorter: (a, b) => a.uploadedby.localeCompare(b.uploadedby),
+      },
+      {
+        title: 'Date',
+        dataIndex: 'date',
+        key: 'date',
+        sorter: (a, b) => a.uploadedby.localeCompare(b.uploadedby),
+
+      },
+      {
+        title: 'Rating',
+        dataIndex: 'rating',
+        key: 'rating',
+        render: rating => <Rate disabled defaultValue={rating} />,
+      },
+      {
+        title: 'Your Rating',
+        dataIndex: 'yourate',
+        key: 'yourate',
+        align:'center',
+        render: (_, record) => (
+          <>
+            <Button  onClick={() => showNameModal(record.name)}><StarOutlined />Rate</Button>
+          </>
+        ),
+
+      },
+      {
+        title: 'Download',
+        dataIndex: 'download',
+        key: 'download',
+        align: 'center',
+        render: (_, record) => (          
+            <Button type="primary" icon={<DownloadOutlined />} onClick={() => console.log("Button clicked!", record.key)}/>
+        )
+      },
+
+      // change visibility only for admin
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+        render: (status, record) => (
+          <Checkbox 
+            checked={status} 
+            onChange={(e) => onChange(e.target.checked, record.key)} 
+          />
+        )
       }  
-  
     ];
-  
+
     const data = [
       {
         key: '1',
-        name: 'Artificial Intelligence',
-        id: 32,
-        department: 'Faculty of Science'        
+        name: 'Programming Basics.pdf',
+        date: '2024-02-28 15:00:29',
+        uploadedby: 'Mariana',
+        rating:3,
+        status: false
       },
       {
         key: '2',
-        name: 'Software Engineering',
-        id: 42,
-        department: 'Faculty of Science'
-      },
-      {
-        key: '3',
-        name: 'Archaelogy',     
-        id: 402,   
-        department: 'Faculty of Humanities'
-        
-      },
-    ]
+        name: 'AProgramming Basics.pdf',
+        date: '2023-02-28 15:00:29',
+        uploadedby: 'AMariana',
+        rating:5,
+        status: true
+
+      }
+      
+    ];
+
+
+    
+    const handleRateChange = (value) => {
+      //setModalRating(value);
+    };
+    
   
+    
     return (
       <div className="container-table">  
         <div className='table-container'>
           <div className='add-course-btn'>
-            <h2>Available Courses</h2>
-            <PlusCircleTwoTone className='add-button'></PlusCircleTwoTone>
-
-
-
-                
+            <h2>Files</h2>                
           </div>
           <Table columns={columns} dataSource={data}/>  
+          <Modal  title="File Rating" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <div className='modal-rating'>
+                <p>What is your rating for {selectedName}?</p>
+                <Rate onChange={handleRateChange} value={modalRating} />
+            </div>
+         
+
+          </Modal> 
+
         </div>  
       </div>
     );
--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -3,13 +3,14 @@ import { Table } from 'antd';
 import "../styles/tables_style.css"
 import StoreContext from '../store/StoreContext';
 import { useNavigate } from 'react-router-dom';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Form, Input, Alert, Space, Table, Tag } from 'antd';
 
 
 
 const Courses = () => {
 
   const navigate = useNavigate();
+  const {TextArea} = Input;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -65,8 +66,27 @@ const Courses = () => {
           <Button type="primary" onClick={showModal}>
             Add course 
           </Button>
-          <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            
+          <Modal title="Add a new course" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <Form>
+              <Form.Item
+                label="Course Name"
+                name="course"
+                rules={[{ required: true, message: 'Please enter the course\'s name' }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Department"
+                name="department"
+                rules={[{ required: true, message: 'Please input the department of the course!' }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Description"
+                name="description"
+                rules={[{ required: true, message: 'Please enter a description!' }]}>
+                <TextArea rows={4} placeholder="Enter a description for the course"/>
+              </Form.Item>
+            </Form>
           </Modal>
         </div>
         <Table columns={columns} dataSource={data}/>
