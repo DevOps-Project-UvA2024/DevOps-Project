f1220e44997a0475d83585f6ab6fcfb9bfe9a2df
Konstantinos Loukaidis <konstantinosalouk@gmail.com>
2024-02-28 14:38:27 +0100

    signout and verification working

--- client/src/pages/NavBar.js
+++ client/src/pages/NavBar.js
@@ -2,7 +2,6 @@ import {Link, useMatch, useResolvedPath} from "react-router-dom"
 import "../styles/navbar.css"
 
 const NavBar = () => {
-    const path = window.location.pathname
     return (
         <nav className="nav">
             <Link to="/" className="site-title">Student Portal</Link>
@@ -23,12 +22,23 @@ const NavBar = () => {
 
 function CustomLink({to,children,...props}){
     
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({path:resolvedPath.pathname})
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({path:resolvedPath.pathname});
+
+    const initSignout = async () => {
+        try {
+            const response = await fetch('api/users/auth/signout');
+            if (!response.ok) throw new Error('Error while signing out');
+            window.location.reload()
+          } catch (error) {
+            console.error(error);
+          }
+    }
+
     return (
     // if pathname is equal to href then the classname is
     <li className={isActive ? "active": ""}>              
-        <Link to={to}{...props}>
+        <Link to={to}{...props} onClick={initSignout}>
             {children}
         </Link>
     </li>)
